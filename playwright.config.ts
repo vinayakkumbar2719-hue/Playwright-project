3a56fa2dbedce8cd8f8e61b065717a17d92583c8
vinayak <vkv707415@gmail.com>
2026-04-07 21:15:32 +0530

    add secretvariable changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.BASEURL,
+    baseURL: process.env.BASE_URL,
     testIdAttribute: 'data-qa',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
